dee099c6dfcfb01279993c0f4f377c0686bdeb50
Sarang Sohail <sarangsohail@gmail.com>
2018-08-02 17:08:23 +0100

    registation setup

--- app/src/main/java/com/example/propertymanagment/MainActivity.java
+++ app/src/main/java/com/example/propertymanagment/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.propertymanagment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // if the user isn't signed in, send them to the 'login activity'
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser == null){
+            Intent startIntent = new Intent(
+                    MainActivity.this, StartActivity.class);
+            startActivity(startIntent);
+            finish();
+        }
     }
 }
