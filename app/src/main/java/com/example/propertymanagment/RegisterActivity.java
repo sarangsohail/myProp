e9bd9ae87c59891d2b62d91207752d56003a1b2b
Sarang Sohail <sarangsohail@gmail.com>
2018-08-02 17:13:38 +0100

    continuing registration process

--- app/src/main/java/com/example/propertymanagment/RegisterActivity.java
+++ app/src/main/java/com/example/propertymanagment/RegisterActivity.java
@@ -1,10 +1,17 @@
 package com.example.propertymanagment;
 
 import android.app.ActionBar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputLayout mDisplayName;
+    private TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+    private Button mCreateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
