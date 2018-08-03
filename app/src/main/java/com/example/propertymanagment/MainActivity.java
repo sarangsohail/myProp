a3c449451019d9e1373443ab1c3ba479517af05d
Sarang Sohail <sarangsohail@gmail.com>
2018-08-04 00:23:29 +0100

    toolbar modifications - small bug needs fixing

--- app/src/main/java/com/example/propertymanagment/MainActivity.java
+++ app/src/main/java/com/example/propertymanagment/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.propertymanagment;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -10,12 +11,18 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mAuth = FirebaseAuth.getInstance();
+
+        //todo - fix why the toolbar isn't showing in the 'app toolbar xml'
+        mToolbar = (Toolbar) findViewById(R.id.main_app_bar);
+        getSupportActionBar(mToolbar);
     }
 
     @Override
