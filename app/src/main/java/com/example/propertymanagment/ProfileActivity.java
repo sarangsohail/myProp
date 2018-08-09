1f2762d9e6023e871ba7f6fd3123e09de8368c81
Sarang Sohail <sarangsohail@gmail.com>
2018-08-09 17:01:32 +0100

    slight additions to profile

--- app/src/main/java/com/example/propertymanagment/ProfileActivity.java
+++ app/src/main/java/com/example/propertymanagment/ProfileActivity.java
@@ -3,8 +3,11 @@ package com.example.propertymanagment;
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,6 +17,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
@@ -35,12 +39,16 @@ public class ProfileActivity extends AppCompatActivity {
 
     private String mCurrent_state;
 
+    private ProgressBar spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        spinner = (ProgressBar) findViewById(R.id.progressBarSingleProfilePic);
+        spinner.setVisibility(View.INVISIBLE);
 
         String userID = getIntent().getStringExtra("user_id");
 
@@ -53,17 +61,23 @@ public class ProfileActivity extends AppCompatActivity {
         mProfileSendReqBtn = (Button) findViewById(R.id.profile_send_req_btn);
         mDeclineBtn = (Button) findViewById(R.id.profile_decline_btn);
 
+        spinner.setVisibility(View.VISIBLE);
+
         mUsersDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 String display_name = dataSnapshot.child("name").getValue().toString();
                 String status = dataSnapshot.child("status").getValue().toString();
-            //    String image = dataSnapshot.child("image").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
 
 
                 mProfileName.setText(display_name);
                 mProfileStatus.setText(status);
+
+                Picasso.get().load(image).placeholder(R.drawable.default_profile_pic).into(mProfileImage);
+                spinner.setVisibility(View.INVISIBLE);
+
             }
 
             @Override
